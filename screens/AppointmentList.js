3f00e5dd24a1efe18a5b4bbbfe8b8e54f9e9508c
Fredse <57021717+Fredse@users.noreply.github.com>
2020-11-17 15:59:37 -0500

    add content   to appointment components

---
+++ screens/AppointmentList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const AppointmentList = () =>{
+    return(
+        <View>
+            <Text>Lista de Citas</Text>
+        </View>
+    )
+}
+
+export default AppointmentList
